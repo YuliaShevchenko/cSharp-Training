d39b0b7799180b3a74edefeb499c4347cb99038d
YuliaShevchenko <ustymets@mail.ru>
2015-12-30 01:18:51 +0300

    string

---
+++ ConsoleApplication1/SubstringInString/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubstringInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter some text");
+            string usersText = Console.ReadLine();
+            Console.WriteLine("Please, enter any part of the text");
+            string partOftext = Console.ReadLine();
+            bool presenceInText = usersText.Contains(partOftext);
+            Console.WriteLine(presenceInText);
+
+            if(presenceInText){
+                int index = usersText.IndexOf(partOftext);
+                Console.WriteLine(index);
+
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
