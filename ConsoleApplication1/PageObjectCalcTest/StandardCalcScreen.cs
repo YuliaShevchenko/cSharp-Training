07d7af59178edb834c49e54d7c149d6cc57b025a
YuliaShevchenko <ustymets@mail.ru>
2016-01-13 02:43:32 +0300

    page object pattern - calc

---
+++ ConsoleApplication1/PageObjectCalcTest/StandardCalcScreen.cs
@@ -0,0 +1,102 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestStack.White;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.MenuItems;
+using TestStack.White.UIItems.WindowItems;
+using TestStack.White.UIItems.WindowStripControls;
+
+namespace PageObjectCalcTest
+{
+   class StandardCalcScreen
+    {
+        private const string APPNAME = "Калькулятор";
+        public Button equalsButton { get; set; }
+        public Button plusButton { get; set; }
+        public Button minusButton { get; set; }
+        public Button multiplyButton { get; set; }
+        public Button divideButton { get; set; }
+
+        public Button digitOneButton { get; set; }
+        public Button digitTwoButton { get; set; }
+        public Button digitThreeButton { get; set; }
+
+        public Label display { get; set; }
+        public Menu helpTab { get; set; }
+        public MenuBar menuBars { get; set; }
+        public Menu menuBar { get; set; }
+        public Menu viewHelp { get; set; }
+
+        public StandardCalcScreen(Application calculator)
+        {
+            Window window = calculator.GetWindow(APPNAME);
+            equalsButton = window.Get<Button>(SearchCriteria.ByAutomationId("121"));
+            plusButton = window.Get<Button>(SearchCriteria.ByAutomationId("93"));
+            display = window.Get<Label>(SearchCriteria.ByAutomationId("158"));
+            // minusButton = window.Get<Button>(SearchCriteria.ByAutomationId("121"));
+            digitOneButton = window.Get<Button>(SearchCriteria.ByText("1"));
+            digitTwoButton = window.Get<Button>(SearchCriteria.ByText("2"));
+            digitThreeButton = window.Get<Button>(SearchCriteria.ByText("3"));
+            window.WaitWhileBusy();
+           // menuBar = window.Get<MenuBar>(SearchCriteria.ByAutomationId("Application"));
+            menuBars = window.MenuBar;
+            menuBar = menuBars.MenuItemBy(SearchCriteria.ByText("MenuBar"));
+           helpTab = menuBars.MenuItemBy(SearchCriteria.ByAutomationId("Item 3"));
+           viewHelp = menuBars.MenuItemBy(SearchCriteria.ByAutomationId("Item 311"));
+        }
+        //TODO: window name -const; rename app. methods rename, enumeration(click operation), buttons
+
+        public void ClickOnHelp()
+        {
+            helpTab.Click();
+            viewHelp.Click();
+        }
+
+        public void ClickOnOneButton()
+        {
+            digitOneButton.Click();
+        }
+        public void ClickOnTwoButton()
+        {
+            digitTwoButton.Click();
+        }
+        public void ClickOnThreeButton()
+        {
+            digitThreeButton.Click();
+        }
+        public void ClickOnPlusButton()
+        {
+            plusButton.Click();
+        }
+        public void ClickOnEqualsButton()
+        {
+            equalsButton.Click();
+        }
+        public string GetDisplayedText()
+        {
+            return display.Text;
+        }
+        public void ClickOnDigitButton(string num)
+        {
+            switch (num)
+            {
+                case "1":
+                    ClickOnOneButton();
+                    break;
+
+                case "2":
+                    ClickOnTwoButton();
+                    break;
+                case "3":
+                    ClickOnThreeButton();
+                    break;
+            }
+        }
+    }
+}
---
+++ ConsoleApplication1/PageObjectCalcTest/UnitTest1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using TestStack.White;
+using TestStack.White.UIItems;
+using PageObjectCalcTest;
+
+namespace PageObjectCalculator
+{
+    [TestClass]
+    public class PageObjectCalcTest
+    {
+        string calculatorAppPath = "calc.exe";
+        //string fullPath = Path.GetFullPath(@"PageObjectCalculator");
+        private const string FILEPATH = @"C:\Users\Jul\Source\Repos\cSharp-Training\ConsoleApplication1\PageObjectCalcTest\Data.csv";
+
+        public TestContext TestContext { get; set; }
+        private Application application;
+
+        [TestInitialize]
+        public void initialize()
+        {
+            application = Application.Launch(calculatorAppPath);
+        }
+
+        [TestCleanup]
+        public void testCleanUp()
+        {
+            application.Close();
+        }
+
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", FILEPATH, "Data#csv", DataAccessMethod.Sequential)]
+        public void AdditionPageObjectTest()
+        {
+            string num1 = TestContext.DataRow[0].ToString();
+            string num2 = TestContext.DataRow[1].ToString();
+            string expectedResult = TestContext.DataRow[2].ToString();
+            StandardCalcScreen calcScreen = new StandardCalcScreen(application);
+            
+            calcScreen.ClickOnDigitButton(num1);
+            calcScreen.ClickOnPlusButton();
+            calcScreen.ClickOnDigitButton(num2);
+            calcScreen.ClickOnEqualsButton();
+            Assert.AreEqual(expectedResult, calcScreen.GetDisplayedText());
+            OpenHelp();
+        }
+
+        public void OpenHelp()
+        {
+            StandardCalcScreen calcScreen = new StandardCalcScreen(application);
+         //   calcScreen.ClickOnHelp();
+        }
+    }
+}
+
+
